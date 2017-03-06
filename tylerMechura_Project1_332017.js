98e30a71c9a744819ebb69280a76d202545da61a
Tyler Mechura <tjm471@nyu.edu>
2017-03-06 11:12:31 -0500

    Trying to get breathBar() working (2)
    Failure - added globals
    
    Added other necessary functions

--- tylerMechura_Project1_332017.js
+++ tylerMechura_Project1_332017.js
@@ -9,22 +9,25 @@ var mainY=0;
 var clr=0;
 var fillBar=0;
 
-var bBarX = 360;
-var bBarY = 80;
+var bBarX = 560;
+var bBarY = 40;
 var bBarW = 100;
 var bBarH = 50;
 
+var xFinal = bBarX+bBarW;
+var xInit = bBarX;
+
 function setup() { 
-  createCanvas(500, 500);
+  createCanvas(700, 500);
 } 
 
 function draw() { 
   background(220);
-    breathBar(bBarX, bBarY, bBarW, bBarH, color(255)); // functionality of the bar coming into play
+    breathBar(bBarX, bBarY, bBarW, bBarH, color(220)); // functionality of the bar coming into play
                        // uses a white rectangle in the same position as 
 
   // main breath bar
-  breathBarStatic(bBarX, bBarY, bBarW, bBarH, color(0, 0, 255));
+  breathBarStatic(bBarX, bBarY, bBarW, bBarH, color(80, 110, 220));
   // end of aesthetics
 }
 
@@ -40,18 +43,42 @@ function breathBarStatic( x, y, w, h, clr){
 }
 // breath bar
 function breathBar (x, y, w, h, clr){ // the aesthetics of the breath bar and the mouse clicked funcionality
+//  fill(clr);
   fill('white');
   noStroke();
-  x = x+w;
-  rect(x, y, w, h);
-  if( mouseIsPressed == false && x > bBarX ){
-    x-=1;
-    rect(x, y, w, h);
-  }else if( mouseIsPressed == true && x < bBarX+bBarW){
+  rect(xFinal, y, w, h);
+
+  if( mouseIsPressed == true && x <= xFinal){
     x+=1;
     rect(x, y, w, h);    
+  }else if( mouseIsPressed == false && x >= xInit ){
+    x-=1;
+    rect(x, y, w, h);
   }
   
+}
+
+function wave(){ // use random within a certain range to create 'waves' for the stick to ride on and the fish to follow
   
+}
+
+function ocean(){ // using function 'waves' to create a neverending ocean
+  
+}
+
+function stick(){ // the stickfigure
+  
+}
+
+function swim(){ // the stickfigure swimming
+  
+}
+
+function fish(){ // the fish that follows the stickfigure through the ocean
+  
+  
+}
+
+function fishSwim(){ // the fish swimming;
   
 }
