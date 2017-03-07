e07a81b95a017669727f18b6f54a24d041c45be6
Tyler Mechura <tjm471@nyu.edu>
2017-03-07 01:22:04 -0500

    Ocean is finished!
    Wave became an "Object"
    
    Ocean became an "Object"
    
    Functions created for both objects
    Wave is called in Ocean in an array that moves
    Move is in wave and called in Ocean
    
    fluff removed

--- tylerMechura_Project1_332017.js
+++ tylerMechura_Project1_332017.js
@@ -17,18 +17,21 @@ var bBarY = 40;
 var bBarW = 100;
 var bBarH = 30;
 
+var ocean;
+
 function setup() {
   createCanvas(700, 500);
 
   // defining commonly used colors
   clrOcean = color(80, 160, 220, 80);
+  ocean = new Ocean(clrOcean);
+  ocean.initialize();
 }
 
 function draw() {
   background(220);
 
-  // the body of the ocean. Never changes
-  ocean(clrOcean);
+  ocean.display();
 
   breathBar(bBarX, bBarY, bBarW, bBarH, color(220)); // functionality of the bar coming into play
   // uses a white rectangle in the same position as 
@@ -48,67 +51,98 @@ function breathBar(x, y, w, h, clr) { // the aesthetics of the breath bar and th
   //  fill(clr);
   fill('blue');
   var wFill = w;
+  // to be filled later:
+  // empty bar when mouse is not pressed
+  // fill bar when mouse is pressed
+}
 
-  //  function keyPressed(){
-  //    if( keyCode == SPACEBAR ){
-  //      
-  //    }
-  //    
-  //    else( keyCode == ESCAPE){
-  //      return false;
-  //    }
-  //    
-  //    
-  //  }
-  while (keyIsPressed === false) { // for loop to decrease breath meeter until empty, then stop
-    if (mouseIsPressed == true && wFill <= w) { // if the mouse is pressed and wFill is less than the full, increase the size of breath rectangle
-      i += 2; // increase the size of the rectangle
-      rect(x, y, i, h); // redraw rectangle      
-    } else if (mouseIsPressed == false) { // if the mouse is not pressed, decrease the size of the rectangle (i), and redraw it
-      rect(x, y, i, h);
+// OBJECT: Wave
+function Wave(anchorX, anchorY, anchorX2, clr) {
+
+  // CONSTRUCTOR
+  /////////////////////////////////////
+  // anchor 1
+  this.x1 = anchorX;
+  this.y1 = anchorY;
+  // first control point
+  this.x2 = random(anchorX, anchorX2); // make sure the point is always between the two anchor points
+  this.y2 = random(oceanY, oceanY - 70); // make sure the point is always above the ocean surface
+  // second control point
+  this.x3 = random(anchorX, anchorX2);
+  this.y3 = random(oceanY, oceanY - 70);
+  // anchor 2
+  this.x4 = anchorX2;
+  this.y4 = anchorY;
+
+
+  // OBJECT FUNCTIONS
+  /////////////////////////////////////
+  // creation of the wave
+  this.display = function () {
+    noStroke();
+    fill(clr);
+    bezier(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3, this.x4, this.y4);
+  }
+  // Move
+  // Called in order to move the wave one pixel to the left
+  this.move = function () {
+    this.x1 -= 1;
+    this.x2 -= 1;
+    this.x3 -= 1;
+    this.x4 -= 1;
+  }
+}
+
+function Ocean(clr) { // using function 'waves' to create a neverending ocean
+
+  // CONSTRUCTOR
+  /////////////////////////////////////
+  this.clr = clr;
+
+  this.waveArray = [];
+  // anchor points for the first wave bezier in waveArray
+  this.anchorX = 0;
+  this.anchorX2 = random(300, 400);
+
+
+  // OBJECT FUNCTIONS
+  /////////////////////////////////////
+  // The Display Loop
+  this.display = function () {
+      // main rectangle for body of ocean
+      noStroke()
+      fill(this.clr);
+      rect(0, oceanY, width, oceanH);
+
+      for (var i = 0; i < this.waveArray.length; i++) {
+        
+        // Wave functions called for each one
+        this.waveArray[i].display();
+        this.waveArray[i].move();
+        
+        // When a wave moves off screen, delete it and add a new one to the end of the array
+        if (this.waveArray[i].x4 < 0) {
+          this.waveArray.splice(0, 1);
+          this.waveArray[this.waveArray.length] = new Wave( 
+            this.waveArray[this.waveArray.length-1].x4+20, 
+            oceanY, 
+            this.waveArray[this.waveArray.length-1].x4+random(300,400), 
+            this.clr);
+        }
+      }
+    }
+    // for loop that puts several waves in an array for looping in function oceanMove()
+  
+  // INITIALIZATION
+  // Used in Setup
+  this.initialize = function () {
+    for (var i = 0; i < 10; i++) {
+      this.waveArray[i] = new Wave(this.anchorX, oceanY, this.anchorX2, this.clr);
+      this.anchorX = this.anchorX2 + 20;
+      this.anchorX2 += random(300, 400);
     }
   }
 }
-
-// bezier wave
-function wave(anchorX, anchorY, anchorX2, clr) {
-  noStroke();
-  fill(clr)
-
-  // first control point
-  var x2 = random(anchorX, anchorX2); // make sure the point is always between the two anchor points
-  var y2 = random(oceanY, oceanY - 70); // make sure the point is always above the ocean surface
-  // second control point
-  var x3 = random(anchorX, anchorX2);
-  var y3 = random(oceanY, oceanY - 70);
-
-  // creation of the wave
-  bezier(anchorX, anchorY, x2, y2, x3, y3, anchorX2, anchorY)
-
-}
-
-function ocean(clr) { // using function 'waves' to create a neverending ocean
-  noStroke();
-  fill(clr);
-  rect(0, oceanY, width, oceanH);
-
-  // the waves of the ocean stored in an array
-  var waveArray = [];
-
-  // anchor points for the first wave bezier in waveArray
-  var anchorX = 0;
-  var anchorX2 = random(300, 400);
-
-
-  for (var i = 0; i < 10; i++) {
-    waveArray[i] = new wave(anchorX, oceanY, anchorX2, clr);
-    anchorX = anchorX2 + 20;
-    anchorX2 += random(300, 400);
-  }
-
-}
-
-function oceanMove(ocean){}
 
 function stick() { // the stickfigure
 
